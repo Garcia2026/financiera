dd29b633151523c6ff30321688222d47b44fc7e4
Garcia2026 <byronloco123@gmail.com>
2025-06-09 22:56:49 -0600

    Fix Firebase storage bucket domain and add newline

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAfg7VAw2F6umMmYBY0r0aL09OHXgqGHgI",
   authDomain: "fincontrol-app.firebaseapp.com",
   projectId: "fincontrol-app",
-  storageBucket: "fincontrol-app.firebasestorage.app",
+  storageBucket: "fincontrol-app.appspot.com",
   messagingSenderId: "714910121771",
   appId: "1:714910121771:web:be6f6fe8dc7de987528f6e",
   measurementId: "G-TB4C5Z2Q29"
